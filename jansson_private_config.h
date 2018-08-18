6753b706f515f1e217eb4d42986939e0d39d6004
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-08-17 22:20:15 -0700

    add link build

--- jansson_private_config.h
+++ jansson_private_config.h
@@ -4,6 +4,11 @@
 #define JANSSON_PRIVATE_CONFIG_H
 
 #define HAVE_STDINT_H 1
+
+
+#if defined __StratifyOS__ || defined __macosx
 #define HAVE_UNISTD_H 1
+#define HAVE_GETPID 1
+#endif
 
 #endif
---
+++ link.cmake
@@ -0,0 +1,39 @@
+
+
+
+#Add sources to the project
+set(SOURCES_PREFIX ${CMAKE_SOURCE_DIR}/jansson/src)
+
+set(SOURCES
+    ${SOURCES_PREFIX}/dump.c
+    ${SOURCES_PREFIX}/error.c
+    ${SOURCES_PREFIX}/hashtable.c
+    ${SOURCES_PREFIX}/hashtable_seed.c
+    ${SOURCES_PREFIX}/load.c
+    ${SOURCES_PREFIX}/memory.c
+    ${SOURCES_PREFIX}/pack_unpack.c
+    ${SOURCES_PREFIX}/strbuffer.c
+    ${SOURCES_PREFIX}/strconv.c
+    ${SOURCES_PREFIX}/utf.c
+    ${SOURCES_PREFIX}/value.c
+    ${SOURCES_PREFIX}/hashtable.h
+    ${SOURCES_PREFIX}/jansson.h
+    ${SOURCES_PREFIX}/jansson_private.h
+    ${SOURCES_PREFIX}/lookup3.h
+    ${SOURCES_PREFIX}/strbuffer.h
+    ${SOURCES_PREFIX}/utf.h
+)
+
+
+set(SOS_LIB_SOURCELIST
+	${SOURCES}
+	jansson_private_config.h
+	jansson_config.h
+	jansson_api.c
+	jansson_api.h)
+
+set(SOS_LIB_INCLUDE_DIRECTORIES ${CMAKE_SOURCE_DIR} ${SOURCES_PREFIX})
+set(SOS_LIB_OPTION "")
+set(SOS_LIB_TYPE release)
+set(SOS_LIB_ARCH link)
+include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-lib.cmake)
