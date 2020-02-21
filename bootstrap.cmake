cbff7a764959999f044d0bc7b3232bdf9ad39622
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-02-21 14:02:45 -0700

    use bootstrap.cmake

---
+++ bootstrap.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required (VERSION 3.6)
+
+# cmake -P ./StratifyOS-jansson.cmake
+
+if( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin" )
+set(SOS_TOOLCHAIN_CMAKE_PATH /Applications/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+endif()
+if( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows" )
+  set(SOS_TOOLCHAIN_CMAKE_PATH C:/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+endif()
+include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-sdk.cmake)
+
+set(JANSSON_DIRECTORY ${CMAKE_SOURCE_DIR}/jansson)
+if(EXISTS  ${JANSSON_DIRECTORY})
+	message("jansson project has already been cloned -- delete to pull again")
+else()
+	sos_sdk_clone_or_pull(${JANSSON_DIRECTORY} https://github.com/akheron/jansson.git ./)
+	sos_sdk_checkout(${JANSSON_DIRECTORY} "2.12")
+endif()
