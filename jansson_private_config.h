a656a5cfce6d65a89374444cc73d842590660d5f
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-05-02 09:03:04 -0600

    Update jansson_private_config.h

--- jansson_private_config.h
+++ jansson_private_config.h
@@ -6,7 +6,7 @@
 #define HAVE_STDINT_H 1
 
 
-#if defined __StratifyOS__ || defined __macosx
+#if defined __StratifyOS__ || defined __macosx || defined __linux
 #define HAVE_UNISTD_H 1
 #define HAVE_GETPID 1
 #endif
