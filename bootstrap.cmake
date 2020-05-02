51f419ae55f51f6c054aaf894a1ce823fe52bb40
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-05-02 08:57:47 -0600

    build for linux

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -1,13 +1,15 @@
 cmake_minimum_required (VERSION 3.6)
 
-# cmake -P ./StratifyOS-jansson.cmake
+# cmake -P ./bootstrap.cmake
 
 if( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin" )
-set(SOS_TOOLCHAIN_CMAKE_PATH /Applications/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+	set(SOS_TOOLCHAIN_CMAKE_PATH /Applications/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows" )
+	set(SOS_TOOLCHAIN_CMAKE_PATH C:/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux" )
+	set(SOS_TOOLCHAIN_CMAKE_PATH /StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
 endif()
-if( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows" )
-  set(SOS_TOOLCHAIN_CMAKE_PATH C:/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
-endif()
+
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-sdk.cmake)
 
 set(JANSSON_DIRECTORY ${CMAKE_SOURCE_DIR}/jansson)
